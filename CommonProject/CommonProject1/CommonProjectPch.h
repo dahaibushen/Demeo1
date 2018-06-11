8f61fd0549f9580b4863b7c49170afd4e406e104
huyiyong <huyiyong@jd.com>
2018-06-11 16:34:27 +0800

    移除引用项目本身的target，同时创建Pch

---
+++ CommonProject/CommonProject1/CommonProjectPch.h
@@ -0,0 +1,13 @@
+//
+//  CommonProjectPch.h
+//  CommonProject
+//
+//  Created by 胡义勇 on 2018/6/11.
+//  Copyright © 2018年 胡义勇. All rights reserved.
+//
+
+#ifndef CommonProjectPch_h
+#define CommonProjectPch_h
+
+
+#endif /* CommonProjectPch_h */
