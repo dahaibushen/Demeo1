8f1fa660e417cb558f82b615441e0e9b503a3c55
huyiyong <huyiyong@jd.com>
2018-06-11 16:54:30 +0800

    清楚不要的文件已经创建新的.a文件

---
+++ CustomProjectNetWork/NetWork/NetWork.h
@@ -0,0 +1,13 @@
+//
+//  NetWork.h
+//  NetWork
+//
+//  Created by 胡义勇 on 2018/6/11.
+//  Copyright © 2018年 胡义勇. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetWork : NSObject
+
+@end
---
+++ CustomProjectNetWork/NetWork/NetWork.m
@@ -0,0 +1,13 @@
+//
+//  NetWork.m
+//  NetWork
+//
+//  Created by 胡义勇 on 2018/6/11.
+//  Copyright © 2018年 胡义勇. All rights reserved.
+//
+
+#import "NetWork.h"
+
+@implementation NetWork
+
+@end
